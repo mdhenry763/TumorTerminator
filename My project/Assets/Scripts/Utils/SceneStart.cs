ab35e2009cfb6d911a450278166b4f62fe3d513a
mdhenry763 <mdhenry763@gmail.com>
2023-10-13 23:40:49 +0200

    Added a scene trigger.

---
+++ My project/Assets/Scripts/Utils/SceneStart.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneStart : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            Debug.Log("Scene Start");
+            EventManager.Instance.LevelStartEvent();
+        }
+    }
+}
