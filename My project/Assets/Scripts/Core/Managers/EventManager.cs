5dba7c7bc9c0871533a461c4d217fb35f8acaa7e
mdhenry763 <mdhenry763@gmail.com>
2023-10-13 12:25:58 +0200

    Added event manager and level manager logic.

--- My project/Assets/Scripts/Core/Managers/EventManager.cs
+++ My project/Assets/Scripts/Core/Managers/EventManager.cs
@@ -1,18 +1,50 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EventManager : MonoBehaviour
+public class EventManager : Singleton<EventManager>
 {
-    // Start is called before the first frame update
-    void Start()
+    public event Action OnLevelStart;
+    public event Action OnManualRead;
+
+    public event Action OnLumpKilled;
+    public event Action OnMainCancerCellDetected;
+    //This event for smaller cnacer cells
+    public event Action OnCancerCellKilled;
+    //This eventis for main cancer cell
+    public event Action OnMainCancerCellKilled;
+    //This will be called once the player has entered the new scene
+    public event Action OnSceneChange;
+
+    public void LevelStartEvent()
     {
-        
+        OnLevelStart?.Invoke();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ManualReadEvent()
     {
-        
+        OnManualRead?.Invoke();
     }
+
+    public void MainCancerCellDetectedEvent()
+    {
+        OnMainCancerCellDetected?.Invoke();
+    }
+
+    public void MainCancerCellKilled()
+    {
+        OnMainCancerCellKilled?.Invoke();
+    }
+
+    public void SceneChangeEvent()
+    {
+        OnSceneChange?.Invoke();
+    }
+
+    public void LumpKilledEvent()
+    {
+        OnLumpKilled?.Invoke();
+    }
+
 }
